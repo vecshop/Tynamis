809b30c8ad1b8af244b1699e86035edc97e22f63
v <123429363+vecshop@users.noreply.github.com>
2025-04-17 07:49:38 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,6 +14,7 @@ const urlsToCache = [
   "/manifest.json",
   "/income_tracker_icons/128.ico",
   "/income_tracker_icons/256.ico",
+  "/income_tracker_icons/win_icon.jpeg",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css",
   "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap",
